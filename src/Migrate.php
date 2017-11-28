6dfc2f6a12177a0fe46db19b51675a22dec30c1b
TJ Draper <tj@buzzingpixel.com>
2017-11-27 22:49:05 -0600

    Initial commit

---
+++ src/Migrate.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author TJ Draper <tj@buzzingpixel.com>
+ * @copyright 2017 BuzzingPixel, LLC
+ * @license Apache-2.0
+ */
+
+namespace felicity\migrate;
+
+use felicity\config\Config;
+
+/**
+ * Class Migrate
+ */
+class Migrate
+{
+}
